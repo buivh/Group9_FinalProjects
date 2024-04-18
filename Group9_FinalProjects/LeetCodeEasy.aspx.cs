2db8cccd484404ff100abc1ba245cb4a88720b6c
Quynh Doan <doanqb@mail.uc.edu>
2024-04-18 15:01:20 -0400

    This is Quynh's Part for Final Project

--- Group9_FinalProjects/LeetCodeEasy.aspx.cs
+++ Group9_FinalProjects/LeetCodeEasy.aspx.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+# Name: Quynh Doan
+# email: doanqb
+# Assignment Title: Final Project 
+# Due Date: 04/23/2024
+# Course: IS 3050
+# Semester/Year: Senior
+# Brief Description: This project is about collaborating using GitHub
+# Citations: https://chat.openai.com/, https://leetcode.com/problems/happy-number/solutions/2912200/happy-number-c-beats-96-fastest/
+# Anything else that's relevant: N/A
+*/
+using Group9_FinalProjects.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +21,27 @@ namespace Group9_FinalProjects
 {
     public partial class LeetCodeEasy : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+
+        protected void cmdRun_Click(object sender, EventArgs e)
         {
 
+            int n;
+            if (int.TryParse(txtHappyNumber.Text, out n))
+            {
+                HappyNumberSolution solver = new HappyNumberSolution();
+                bool solutions = solver.IsHappy(n);
+                String solutionString;
+                if (solutions) solutionString = n.ToString() + " is a happy number";
+                else solutionString = n.ToString() + " is not a happy number";
+                lblSolution.Text = solutionString;
+            }
+            else
+            {
+                lblSolution.Text = "Invalid input. Please enter a valid integer.";
+            }
+
         }
+
+       
     }
 }
